b01156d40b4e1ebba735faf1f2f1379d90c54b3d
restodruid1 <106450663+restodruid1@users.noreply.github.com>
2024-12-20 12:52:43 -0800

    ch.2 & 2-1.c

---
+++ Chapter_2/2-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <limits.h>
+
+
+int main(void) {
+	printf("Max Int: %d\n", INT_MAX);
+	printf("Min Int: %d\n", INT_MIN);
+	printf("Unsigned short int Max: %u\n", USHRT_MAX);
+	//printf("Unsigned short int Min: %hu\n", USHRT_MIN); this is 0
+	printf("Char Max: %d\n", CHAR_MAX);
+	printf("Char Min: %d\n", CHAR_MIN);
+	printf("Long Long Max: %lld\n", LLONG_MAX);
+	printf("Long Long Min: %lld\n", LLONG_MIN);
+}
+
+
+
